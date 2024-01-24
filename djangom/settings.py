750986fae67e1750236465c6ff5f58b370509520
Matheus Silva Gastaldi <matheusdasilvagastaldi@gmail.com>
2024-01-24 14:33:12 -0300

    Setimo commit, Debug true

--- djangom/settings.py
+++ djangom/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!oim&vq7v_&*oa!9y5ipx)n4+wju+ddp2fqnm!+re%ojg=eok)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://djangoformsmysql-production.up.railway.app/']
 
 
 # Application definition
@@ -75,7 +75,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangom.wsgi.application'
 
-CSRF_TRUSTED_ORIGINS = ['https://djangoformsmysql-production.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://djangoformsmysql-production.up.railway.app/', 'https://djangoformsmysql-production.up.railway.app/admin', 'https://djangoformsmysql-production.up.railway.app/produto']
 
 
 # Database
