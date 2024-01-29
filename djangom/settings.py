b7fd22104dd081d075892666366f4db02c79e740
Matheus Silva Gastaldi <matheusdasilvagastaldi@gmail.com>
2024-01-28 21:02:05 -0300

    Trocando whitenoise por DJ-static, Dbug = False

--- djangom/settings.py
+++ djangom/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!oim&vq7v_&*oa!9y5ipx)n4+wju+ddp2fqnm!+re%ojg=eok)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -54,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+# 'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ROOT_URLCONF = 'djangom.urls'
 
